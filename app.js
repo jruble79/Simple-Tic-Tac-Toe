854a6cf58047ebcfb793dd792f21aee7579ce0b3
Jeff Ruble <jeff.ruble@gmail.com>
2021-08-24 16:47:28 -0400

    Moves tilesArray, playerMark, checkForWin, checkForTie and changePlayer into Gameboard class. Updates all relevant calls to the new location

--- app.js
+++ app.js
@@ -4,22 +4,54 @@ class Tile {
     }
 }
 
-let tilesArray = [
-    [new Tile(), new Tile(), new Tile()],
-    [new Tile(), new Tile(), new Tile()],
-    [new Tile(), new Tile(), new Tile()]
-];
-
 class Gameboard {
-    constructor(){}
-    get gameboard() {
-        return this.createGameboard();
-    }
+    
+    tilesArray = [
+        [new Tile(), new Tile(), new Tile()],
+        [new Tile(), new Tile(), new Tile()],
+        [new Tile(), new Tile(), new Tile()]
+    ];
+
+    playerMark = 'X';
+
+    constructor() {}
+
     createGameboard() {
-        return console.table(tilesArray);
+        console.table(this.tilesArray);
     }
+
     addPlayerMark(playerMark, row, col) {
-        return tilesArray[row][col].playerMark = playerMark;
+        this.tilesArray[row][col].playerMark = playerMark;
+    }
+
+    checkForWin(playerMark, row, col) {
+        if (this.tilesArray[row][0].playerMark === playerMark && this.tilesArray[row][1].playerMark === playerMark && this.tilesArray[row][2].playerMark === playerMark) {
+            return true;
+        } else if (this.tilesArray[0][col].playerMark === playerMark && this.tilesArray[1][col].playerMark === playerMark && this.tilesArray[2][col].playerMark === playerMark) {
+            return true;
+        } else if (this.tilesArray[0][0].playerMark === playerMark && this.tilesArray[1][1].playerMark === playerMark && this.tilesArray[2][2].playerMark === playerMark) {
+            return true;
+        } else if (this.tilesArray[2][0].playerMark === playerMark && this.tilesArray[1][1].playerMark === playerMark && this.tilesArray[0][2].playerMark === playerMark) {
+            return true;
+        }
+    }
+
+    checkForTie() {
+        if (
+            this.tilesArray[0][0].playerMark !== '—' && this.tilesArray[0][1].playerMark !== '—' && this.tilesArray[0][2].playerMark !== '—' &&
+            this.tilesArray[1][0].playerMark !== '—' && this.tilesArray[1][1].playerMark !== '—' && this.tilesArray[1][2].playerMark !== '—' && 
+            this.tilesArray[2][0].playerMark !== '—' && this.tilesArray[2][1].playerMark !== '—' && this.tilesArray[2][2].playerMark !== '—'
+        ) {
+            return true;
+        }
+    }
+
+    changePlayer() {
+        if (this.playerMark === 'X') {
+            this.playerMark = 'O';
+        } else if (this.playerMark === 'O') {
+            this.playerMark = 'X';
+        };
     }
     
 }
@@ -32,9 +64,9 @@ class Gameboard {
 function gameCycle() {
 
     // Player marks tile
-    let row = parseInt( prompt(`${playerMark} Choose your row`) );
-    let col = parseInt( prompt(`${playerMark} Choose your column`) );
-    thisGame.addPlayerMark(playerMark, row, col);
+    let row = parseInt( prompt(`${thisGame.playerMark} Choose your row`) );
+    let col = parseInt( prompt(`${thisGame.playerMark} Choose your column`) );
+    thisGame.addPlayerMark(thisGame.playerMark, row, col);
 
     // Check for win condition
         // First checks for winning arrangement
@@ -42,49 +74,17 @@ function gameCycle() {
     // If either are true, announces winner or tie and ends the game
     // If both are false, continues the game
 
-    if ( checkForWin(row, col) === true) {
-        console.log(`${playerMark} wins!`);
+    if ( thisGame.checkForWin(thisGame.playerMark, row, col) === true) {
+        console.log(`${thisGame.playerMark} wins!`);
         return; // Ends the game
-    } else if ( checkForTie() === true ) {
+    } else if ( thisGame.checkForTie(thisGame.playerMark) === true ) {
         console.log('Tie! No winner!');
         return; // Ends the game
     } else {
         // Else change player and continue game cycle
-        changePlayer();
+        thisGame.changePlayer();
         gameCycle();
     }
-}
-
-function checkForWin(row, col) {
-    
-    if (tilesArray[row][0].playerMark === playerMark && tilesArray[row][1].playerMark === playerMark && tilesArray[row][2].playerMark === playerMark) {
-        return true;
-    } else if (tilesArray[0][col].playerMark === playerMark && tilesArray[1][col].playerMark === playerMark && tilesArray[2][col].playerMark === playerMark) {
-        return true;
-    } else if (tilesArray[0][0].playerMark === playerMark && tilesArray[1][1].playerMark === playerMark && tilesArray[2][2].playerMark === playerMark) {
-        return true;
-    } else if (tilesArray[2][0].playerMark === playerMark && tilesArray[1][1].playerMark === playerMark && tilesArray[0][2].playerMark === playerMark) {
-        return true;
-    }
-
-}
-
-function checkForTie() {
-    if (
-        tilesArray[0][0].playerMark !== '—' && tilesArray[0][1].playerMark !== '—' && tilesArray[0][2].playerMark !== '—' &&
-        tilesArray[1][0].playerMark !== '—' && tilesArray[1][1].playerMark !== '—' && tilesArray[1][2].playerMark !== '—' && 
-        tilesArray[2][0].playerMark !== '—' && tilesArray[2][1].playerMark !== '—' && tilesArray[2][2].playerMark !== '—'
-    ) {
-        return true;
-    }
-}
-
-function changePlayer() {
-    if (playerMark === 'X') {
-        playerMark = 'O';
-    } else if (playerMark === 'O') {
-        playerMark = 'X';
-    };
 }
 
 ////////////////////////////////////////////////////////////
@@ -94,9 +94,9 @@ function changePlayer() {
 
 // Create new board
 let thisGame = new Gameboard;
-thisGame.gameboard;
+thisGame.createGameboard();
 
 // X starts the game
-let playerMark = 'X';
+// let playerMark = 'X';
 
 gameCycle();
