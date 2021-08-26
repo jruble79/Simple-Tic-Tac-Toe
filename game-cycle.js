12c3ab3c08b19158b8da80981c9bfab2948d94ef
Jeff Ruble <jeff.ruble@gmail.com>
2021-08-26 13:27:02 -0400

    markTile and endgameCheck functions moved here

---
+++ game-cycle.js
@@ -0,0 +1,34 @@
+
+////////////////////////////////////////////////////////////
+// THE GAME CYCLE
+////////////////////////////////////////////////////////////
+
+function markTile(thisGame) {
+    // Player marks tile
+    let row = parseInt( prompt(`${thisGame.playerMark} Choose your row`) );
+    let col = parseInt( prompt(`${thisGame.playerMark} Choose your column`) );
+    thisGame.addPlayerMark(thisGame.playerMark, row, col);
+
+    // Advance to endgame check
+    endgameCheck(row, col);
+}
+
+function endgameCheck(row, col, thisGame) {
+    // Check for endgame condition
+        // First checks for winning arrangement
+        // Then checks for a tie
+    // If either are true, announces winner or tie and ends the game
+    // If both are false, continues the game
+
+    if ( thisGame.checkForWin(thisGame.playerMark, row, col) === true) {
+        console.log(`${thisGame.playerMark} wins!`);
+        return; // Ends the game
+    } else if ( thisGame.checkForTie(thisGame.playerMark) === true ) {
+        console.log('Tie! No winner!');
+        return; // Ends the game
+    } else {
+        // Else change player and continue game cycle
+        thisGame.changePlayer();
+        markTile();
+    }
+}
